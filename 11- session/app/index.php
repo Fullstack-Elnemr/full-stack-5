a7b1ef9a2cdc5a00e12ca76cbf54fddc0e2c4a42
pro.ahmedelnemr <pro.ahmedelnemr@gmail.com>
2018-10-13 11:08:18 +0200

    session11

---
+++ 11- session/app/index.php
@@ -0,0 +1,36 @@
+<?php
+/*
+$number1 = -5.5;
+$number2 = 5;
+
+$result = $number1 . $number2;
+
+echo $result;
+*/
+#inlien
+//inline
+
+//$male = false * false + true + '5p' + 'u888' - "12.5f";
+//echo $male;
+
+//define('a', 5);
+//define('A', 7, true);
+//$connect = DATABASE_CONNECTION;
+//echo a;
+
+//echo __LINE__;
+
+//$sum = 5;
+//$sum = $sum + 1;
+//$sum += 5;
+//$sum++;
+//$y = $sum++;
+//$z = ++$sum;
+
+//echo "sum = $sum ---- z = $z";
+
+//echo 5 !== "5";
+//echo 1 || 1;
+$x = 5 + 2 * 4;
+$y = 5 + 2 * 4;
+echo $x++ + $y--;
