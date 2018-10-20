ba27ff7904b60aa2bbf95083ec1f6839843bed3f
pro.ahmedelnemr <pro.ahmedelnemr@gmail.com>
2018-10-20 12:09:05 +0200

    session 13

--- 13- session/session13/app/login.php
+++ 13- session/session13/app/login.php
@@ -30,34 +30,45 @@ if (isset($_GET['email'], $_GET['password'])) {
 
 ?>
 
-    <div class="container">
-        <div class="content center-block">
+<div class="container">
+    <div class="col-md-6 col-md-offset-3">
 
-            <div class="errors">
-                <?php print_r($errors); ?>
-            </div>
-
-            <form method="get">
-                <div class="form-group">
-                    <label for="">Email</label>
-                    <input type="email"
-                           class="form-control"
-                           placeholder="email"
-                           autocomplete="off"
-                           value="<?php if(isset($_GET['email'])) echo $_GET['email']; ?>"
-                           name="email">
-                </div>
-                <div class="form-group">
-                    <label for="">Password</label>
-                    <input type="password"
-                           class="form-control"
-                           placeholder="00000"
-                           autocomplete="off"
-                           name="password">
-                </div>
-                <input type="submit" class="btn btn-success" name="login">
-            </form>
+        <div class="errors">
+            <?php
+            if(!empty($errors)) {
+                for($i = 0; $i < count($errors); $i++){
+                    ?>
+                    <p class="bg-danger">
+                        <?php
+                        echo $errors[$i];
+                        ?>
+                    </p>
+                    <?php
+                }
+            }
+            ?>
         </div>
+        <form method="get">
+            <div class="form-group">
+                <label for="">Email</label>
+                <input type="email"
+                       class="form-control"
+                       placeholder="email"
+                       autocomplete="off"
+                       value="<?php if(isset($_GET['email'])) echo $_GET['email']; ?>"
+                       name="email">
+            </div>
+            <div class="form-group">
+                <label for="">Password</label>
+                <input type="password"
+                       class="form-control"
+                       placeholder="00000"
+                       autocomplete="off"
+                       name="password">
+            </div>
+            <input type="submit" class="btn btn-success" name="login">
+        </form>
     </div>
+</div>
 
 <?php include 'templates/footer.php'; ?>
